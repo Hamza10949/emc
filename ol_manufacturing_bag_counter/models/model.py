c441443402920282813cb8238593f6f7582554bf
Usama Shahid <itisusamashahid@gmail.com>
2022-11-15 12:26:57 +0500

    added bag counter to workorder model and tablet view.

--- ol_manufacturing_bag_counter/models/model.py
+++ ol_manufacturing_bag_counter/models/model.py
@@ -5,10 +5,18 @@ from odoo.exceptions import UserError
 
 
 
-class ext(models.Model):
+class extproduction(models.Model):
      _inherit = "mrp.production"
      ol_bags = fields.Char(compute='_compute_ol_bags', string='Bags')
      
      @api.depends('product_qty')
      def _compute_ol_bags(self):
         self.ol_bags=self.product_qty/self.bom_id.product_qty
+
+class extworkorder(models.Model):
+    _inherit = "mrp.workorder"
+    ol_bags = fields.Char(compute='_compute_ol_bags', string='Bags')
+
+    @api.depends('')
+    def _compute_ol_bags(self):
+        self.ol_bags=self.production_id.ol_bags
