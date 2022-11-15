4a2f2f3420ba1bf318038f11f7f876135a2406ed
Usama Shahid <itisusamashahid@gmail.com>
2022-11-15 11:41:19 +0500

    added ol_manufacturing_bag_counter
    added ol_manufacturing_bag_counter

---
+++ ol_manufacturing_bag_counter/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Manufacturing Bag Counter',
+
+    "author": "Usama Shahid",
+    'version': '0.1',
+    'category': 'Manufacturing Customization',
+    'sequence': 95,
+    'summary': 'Adds a field on manufacturing order which calculates number of bags.',
+    'description': "Adds a field on manufacturing order which calculates number of bags.",
+    'website': '',
+    'images': [
+    ],
+    'depends': ["mrp",
+        
+    ],
+    'data': [
+        
+        "views/view.xml",
+        
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'qweb': [
+    ],
+    'license': 'LGPL-3',
+}
