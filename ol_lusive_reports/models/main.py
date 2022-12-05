1f923882457353bf4d7f49aa61823a9b776d259e
Ali-Shahab <alishahab9090@gmail.com>
2022-12-05 16:25:20 +0500

    Reports

---
+++ ol_lusive_reports/models/main.py
@@ -0,0 +1,21 @@
+from odoo import models, fields,api
+from odoo.exceptions import UserError
+import base64
+import requests
+import datetime
+
+
+class inheritincompany(models.Model):
+    _inherit = 'stock.production.lot'
+
+    process = fields.Char(string='Process Code')
+    psi = fields.Char(string='PSI')
+    sample_collection = fields.Char(string='QA Sample Collected')
+    technician = fields.Char(string='Technician')
+
+class inheritincompany(models.Model):
+    _inherit = 'stock.picking'
+
+    shipping_instruction = fields.Selection([('Overnight', 'Overnight'), ('SecondDay', 'Second Day'), ('Custom', 'Custom')])
+    prepared_by = fields.Char(string='Prepared By')
+    delivered_by = fields.Char(string='Delivered By')
