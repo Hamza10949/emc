424b84212d22a2da6f5b512ece25d8b1533e013b
Ali-Shahab <alishahab9090@gmail.com>
2022-12-05 17:16:18 +0500

    report

---
+++ ol_lusive_reports/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# Part of Odoolution.
+{
+    "name": "EMC Reports",
+
+    "author": "Odoolution",
+
+    "license": "OPL-1",
+
+    "version": "15.0.1",
+
+    "depends": [
+        'stock','sale_management'
+    ],
+
+    "data": [
+        'views/main.xml',
+        'reports/report.xml',
+        'reports/lot_traveller.xml',
+        # 'reports/bill_landing.xml',
+        'reports/bill_landing2.xml',
+
+    ],
+    "images": [ ],
+    "auto_install": False,
+    "application": True,
+    "installable": True,
+
+}
